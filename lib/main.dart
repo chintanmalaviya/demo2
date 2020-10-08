325711897ddfe1a23fbedd11b5ca3bf17a9a7e91
chintanmalaviya <69718271+chintanmalaviya@users.noreply.github.com>
2020-10-08 19:25:04 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyAppState extends State<MyApp>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Animated Container Che"),
+        title: Text("Animated Container"),
       ),
       body: Center(
         child: Column(
